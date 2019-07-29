031ab8d9dace0f3b577adf256ca0344b0a55271d
Jameka Echols <jameka.echols@gmail.com>
2019-07-28 22:30:08 -0500

    initial

---
+++ sneakers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sneakers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
