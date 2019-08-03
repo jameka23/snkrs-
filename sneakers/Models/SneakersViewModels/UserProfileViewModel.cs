69983ffa121c06ffa8af690f8cd4810771121ae2
Jameka Echols <jameka.echols@gmail.com>
2019-08-02 23:20:18 -0500

    saving adding a review

---
+++ sneakers/Models/SneakersViewModels/UserProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sneakers.Models.SneakersViewModels
+{
+    public class UserProfileViewModel
+    {
+        public ApplicationUser User { get; set; }
+
+        public List<Sneaker> AllSneakers { get; set; }
+    }
+}
